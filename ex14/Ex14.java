a294b52a18245d8cfcf082d28c400a71fd345770
carlosricov <carlos.ricoveri.cpe@gmail.com>
2021-12-16 21:39:44 -0500

    Adds comments

--- ex14/Ex14.java
+++ ex14/Ex14.java
@@ -1,23 +1,32 @@
 import java.io.*;
 import java.util.*;
 
+// This code is unoptimized and currently fails part 2 of the solution.
 public class Ex14 {
+  // Steps to be processed for the polymer.
+  static final int steps = 10;
+
   public static void main(String[] args) throws Exception {
     File f = new File("input.txt");
     Scanner in = new Scanner(f);
 
     Map<String, String> map = new HashMap<>();
+
+    // Template is given in the first line of the file.
     String template = in.nextLine();
 
+    // File read and parse.
     while (in.hasNext()) {
       String line = in.nextLine();
       String[] strArr = line.split(" -> ");
 
+      // Letter pairs are correlated with the insertion letter using a map.
       map.put(strArr[0], strArr[1]);
     }
 
     // Steps = limit.
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < steps; i++) {
+      // For easy insertion, use list.
       char[] arr = template.toCharArray();
       List<Character> list = new ArrayList<>();
 
@@ -32,6 +41,7 @@ public class Ex14 {
       newline();
     }
 
+    // Overflow will be expected if using 32 bit values.
     long[] maxMin = countMax(template);
 
     System.out.println(template);
@@ -40,13 +50,19 @@ public class Ex14 {
 
   }
 
+  // Inserts letters based on the rules from the file.
   private static String processList(String template, List<Character> list, Map<String, String> map) {
     String newTemplate = "";
+
+    // Prevents repeat of letters in our polymer.
     boolean firstPass = true;
 
+    // Limit is - 1 since we are processing letter pairs.
     for (int i = 0; i < list.size() - 1; i++) {
+      // Create pair.
       String sub = String.valueOf(list.get(i)) + String.valueOf(list.get(i + 1));
 
+      // Check if we need to insert a letter due to the pair.
       if (map.containsKey(sub)) {
         if (firstPass) {
           newTemplate += String.valueOf(list.get(i)) + map.get(sub) + String.valueOf(list.get(i + 1));;
@@ -68,6 +84,7 @@ public class Ex14 {
     return newTemplate;
   }
 
+  // Converts a list of characters to a string.
   private static String listToStr(List<Character> list) {
     String result = "";
 
@@ -77,18 +94,24 @@ public class Ex14 {
     return result;
   }
 
+  // Calculates max and min letter frequencies.
   private static long[] countMax(String template) {
     char[] arr = template.toCharArray();
 
+    // Initialized with garbage value.
     char maxLetter = '1';
     char minLetter = '1';
-    long[] a = new long[26];
     long min = 0;
+
+    // 26 letters in the alphabet.
+    long[] a = new long[26];
 
     for (char c : arr) {
       a[c - 'A']++;
     }
 
+    // Max frequency should be at the end of the array.
+    // Min frequency should be at the first non-zero value.
     Arrays.sort(a);
 
     for (long num : a) {
@@ -100,10 +123,13 @@ public class Ex14 {
 
     System.out.println("Max letter: " + maxLetter + " Min Letter: " + minLetter);
     System.out.println("Max letter: " + a[a.length - 1] + " Min Letter: " + min);
+
+    // Return as array to keep track of both min and max.
     return new long[]{a[a.length - 1], min};
 
   }
 
+  // Easier way to print a new line.
   private static void newline() {
     System.out.println(" ");
   }
